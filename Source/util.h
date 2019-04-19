f8aafdcb7aaf6bc1e61dd7a340e1efe09b2be00e
Nelson Chen <nelson.chen@mobivity.com>
2019-04-19 15:11:24 -0700

    Set logset option and larger strlen

--- Source/util.h
+++ Source/util.h
@@ -1,15 +1,15 @@
 /*
  * util.h
- * 
+ *
  * This file is a part of NSIS.
- * 
+ *
  * Copyright (C) 1999-2015 Nullsoft and Contributors
- * 
+ *
  * Licensed under the zlib/libpng license (the "License");
  * you may not use this file except in compliance with the License.
- * 
+ *
  * Licence details can be found in the file COPYING.
- * 
+ *
  * This software is provided 'as-is', without any express or implied
  * warranty.
  */
@@ -17,6 +17,7 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
+#include <stddef.h> // for longer strlen
 #include <string> // for std::string
 
 #include "boost/scoped_ptr.hpp" // for boost::scoped_ptr
